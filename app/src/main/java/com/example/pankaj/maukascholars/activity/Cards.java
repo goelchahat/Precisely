5323eda60120c39257ecdf520b9fc7d974323944
Pankaj Baranwal <pankajisadeveloper@gmail.com>
2017-11-03 18:01:51 +0530

    First commit of redefined app

---
+++ app/src/main/java/com/example/pankaj/maukascholars/activity/Cards.java
@@ -0,0 +1,16 @@
+package com.example.pankaj.maukascholars.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.pankaj.maukascholars.R;
+
+
+public class Cards extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cards);
+    }
+}
---
+++ app/src/main/java/com/example/pankaj/maukascholars/activity/Filters.java
@@ -0,0 +1,15 @@
+package com.example.pankaj.maukascholars.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.pankaj.maukascholars.R;
+
+public class Filters extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filters);
+    }
+}
